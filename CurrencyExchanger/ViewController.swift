ad2add6d4c33445be0eb6f1bf33e51f1c83886c5
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-01-07 16:18:11 +0800

    Initial Commit

---
+++ CurrencyExchanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyExchanger
+//
+//  Created by Neil Francis Hipona on 1/7/20.
+//  Copyright © 2020 Neil Francis Hipona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
